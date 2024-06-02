ac5cc165674e15f9d5a7c1757110405a07fa8a4c
Fahril <fahrilmaula005@gmail.com>
2024-06-02 14:59:17 +0700

    update controller

--- controllers/fuzzy_opencv_lfr_fixed_amin/fuzzy_opencv_lfr_fixed_amin.py
+++ controllers/fuzzy_opencv_lfr_fixed_amin/fuzzy_opencv_lfr_fixed_amin.py
@@ -1,4 +1,5 @@
 from controller import Robot
+from controller import Display
 import numpy as np
 import cv2
 import skfuzzy as fuzz
@@ -6,60 +7,45 @@ import skfuzzy.control as ctl
 
 def get_center(im):
     gray = cv2.cvtColor(im, cv2.COLOR_RGB2GRAY)
+    # ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+    # contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
-
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(im, contours, -1, (0, 0, 255), 5)
 
     cX = 0
     cY = 0
-    valid_contours = []
-    height, width = im.shape[:2]
-    for c in contours:
-        area = cv2.contourArea(c)
-        if 500 < area < 5000:  # Filter contours by area size
-            x, y, w, h = cv2.boundingRect(c)
-            if y + h > height // 2:  # Only consider contours in the bottom half of the image
-                valid_contours.append(c)
-
-    if valid_contours:
-        c = max(valid_contours, key=cv2.contourArea)  # Select the largest valid contour
+    if len(contours) > 0:
+        c = contours[0]
         M = cv2.moments(c)
         if M["m00"] != 0:
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
             cv2.circle(im, (cX, cY), 3, (0, 255, 0), -1)
-
-        # Draw only valid contours
-        cv2.drawContours(im, valid_contours, -1, (0, 0, 255), 5)
-
-    # Center line
-    center_x = width // 2
-    cv2.line(im, (center_x, 0), (center_x, height), (255, 0, 0), 2)
-
     return cX, cY, im
 
-
+# Define fuzzy system
 # Input
-error = ctl.Antecedent(np.arange(-210, 211, 1), 'Error')
-delta_error = ctl.Antecedent(np.arange(-210, 211, 1), 'Delta Error')
+error = ctl.Antecedent(np.arange(-160, 161, 1), 'Error')
+delta_error = ctl.Antecedent(np.arange(-160, 161, 1), 'Delta Error')
 
 # Output
 left_motor_speed = ctl.Consequent(np.arange(0, 6.29, 0.1), 'Left Motor Speed')
 right_motor_speed = ctl.Consequent(np.arange(0, 6.29, 0.1), 'Right Motor Speed')
 
 # Membership functions for error
-error['highly_negative'] = fuzz.trimf(error.universe, [-210, -210, -105])
-error['negative'] = fuzz.trimf(error.universe, [-210, -105, 0])
-error['mid'] = fuzz.trimf(error.universe, [-105, 0, 105])
-error['positive'] = fuzz.trimf(error.universe, [0, 105, 210])
-error['highly_positive'] = fuzz.trimf(error.universe, [105, 210, 210])
+error['highly_negative'] = fuzz.trimf(error.universe, [-160, -160, -80])
+error['negative'] = fuzz.trimf(error.universe, [-160, -80, 0])
+error['mid'] = fuzz.trimf(error.universe, [-80, 0, 80])
+error['positive'] = fuzz.trimf(error.universe, [0, 80, 160])
+error['highly_positive'] = fuzz.trimf(error.universe, [80, 160, 160])
 
 # Membership functions for delta_error
-delta_error['highly_negative'] = fuzz.trimf(delta_error.universe, [-210, -210, -105])
-delta_error['negative'] = fuzz.trimf(delta_error.universe, [-210, -105, 0])
-delta_error['mid'] = fuzz.trimf(delta_error.universe, [-105, 0, 105])
-delta_error['positive'] = fuzz.trimf(delta_error.universe, [0, 105, 210])
-delta_error['highly_positive'] = fuzz.trimf(delta_error.universe, [105, 210, 210])
+delta_error['highly_negative'] = fuzz.trimf(delta_error.universe, [-160, -160, -80])
+delta_error['negative'] = fuzz.trimf(delta_error.universe, [-160, -80, 0])
+delta_error['mid'] = fuzz.trimf(delta_error.universe, [-80, 0, 80])
+delta_error['positive'] = fuzz.trimf(delta_error.universe, [0, 80, 160])
+delta_error['highly_positive'] = fuzz.trimf(delta_error.universe, [80, 160, 160])
 
 # Membership functions for motor speed
 left_motor_speed['very_slow'] = fuzz.trimf(left_motor_speed.universe, [0, 0, 1.57])
@@ -108,19 +94,23 @@ rules = [
 ]
 
 # Control system
-rms_ctrl = ctl.ControlSystem(rules)
-lms_ctrl = ctl.ControlSystem(rules)
+left_motor_speed_ctrl = ctl.ControlSystem(rules)
+right_motor_speed_ctrl = ctl.ControlSystem(rules)
 
-# Simulation
-rms_sim = ctl.ControlSystemSimulation(rms_ctrl)
-lms_sim = ctl.ControlSystemSimulation(lms_ctrl)
+left_motor_speed_sim = ctl.ControlSystemSimulation(left_motor_speed_ctrl)
+right_motor_speed_sim = ctl.ControlSystemSimulation(right_motor_speed_ctrl)
 
 # Initialize Robot
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 
 cam = robot.getDevice("camera")
+# cam.setWidth(128)
+# cam.setHeight(128)
 cam.enable(timestep)
+
+# display = Display("display")
+# display.attachCamera(cam)
 
 left_motor = robot.getDevice("left wheel motor")
 right_motor = robot.getDevice("right wheel motor")
@@ -130,52 +120,46 @@ left_motor.setVelocity(0.0)
 right_motor.setVelocity(0.0)
 
 # Main loop:
-previous_error = 0
+previous_error_value = 0
 max_speed = 6.28
 
 while robot.step(timestep) != -1:
     img = cam.getImageArray()
-    if img is not None:
-        img = np.asarray(img, dtype=np.uint8)
-        img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
-        img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-        img = cv2.resize(img, (0, 0), fx=3.5, fy=3.5)
-        crop_img = cv2.flip(img, 1)
+    img = np.asarray(img, dtype=np.uint8)
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+    img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    img = cv2.resize(img, (0, 0), fx=3.5, fy=3.5)
+    crop_img = cv2.flip(img, 1)
 
-        cX, cY, im = get_center(crop_img)
-        cv2.imshow("Image", im)
-        cv2.waitKey(15)
-        
-        width = im.shape[1]
-        center_point = width // 2
-        error = cX - center_point
-        delta_error = error - previous_error
-        previous_error = error
+    cX, cY, im = get_center(crop_img)
+    # display.imagePaste(im, 0, 0, False)
+    cv2.imshow("Image", im)
+    cv2.waitKey(33)
+    
+    width = im.shape[1]
+    center_point = width // 2
+    error_value = cX - center_point
+    delta_error_value = error_value - previous_error_value
+    previous_error_value = error_value
 
-        # Fuzzy inference
-        rms_sim.input['Error'] = error
-        rms_sim.input['Delta Error'] = delta_error
-        lms_sim.input['Error'] = error
-        lms_sim.input['Delta Error'] = delta_error
+    # Fuzzy inference
+    left_motor_speed_sim.input['Error'] = error_value
+    left_motor_speed_sim.input['Delta Error'] = delta_error_value
+    right_motor_speed_sim.input['Error'] = error_value
+    right_motor_speed_sim.input['Delta Error'] = delta_error_value
 
-        rms_sim.compute()
-        lms_sim.compute()
+    left_motor_speed_sim.compute()
+    right_motor_speed_sim.compute()
+    
+    print(f"Error: {error_value}")
+    print(f"Delta Error: {delta_error_value}")
 
-        right_speed = rms_sim.output['Right Motor Speed']
-        left_speed = lms_sim.output['Left Motor Speed']
+    left_motor_speed_output = left_motor_speed_sim.output['Left Motor Speed']
+    right_motor_speed_output = right_motor_speed_sim.output['Right Motor Speed']
 
-        # Set motor velocities
-        left_motor.setVelocity(left_speed)
-        right_motor.setVelocity(right_speed)
-        
-        # Debug
-        print("=------------------------------=")
-        print(f"Center point: {center_point}")
-        print(f"Contour center X: {cX}")
-        print(f"Error: {error}")
-        print(f"Delta Error: {delta_error}")
-        print(f"Left Motor Speed: {left_speed}")
-        print(f"Right Motor Speed: {right_speed}")
+    # Set motor velocities
+    left_motor.setVelocity(left_motor_speed_output)
+    right_motor.setVelocity(right_motor_speed_output)
 
 # Exit cleanup
 cv2.destroyAllWindows()
